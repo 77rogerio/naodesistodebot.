24bad905966c2bbcc6dfb2cdd7cf9d28cc9ffabc
77rogerio <79286333+77rogerio@users.noreply.github.com>
2021-02-21 01:32:25 -0400

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
-#!/usr/bin/bash
-
-apt-get update
-apt-get upgrade
-apt-get install -y nodejs libwebp ffmpeg wget tesseract
-wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
-npm install
-
-echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
+apt update
+apt upgrade
+apt install
+pkg update && pkg upgrade
+pkg install wget
+pkg install ffmpeg
+pkg install nodejs
+pkg install tesseract
+npm i -g cwebp
+npm i -g ytdl 
+npm i
+npm i got
